65545860566d6fb3d270e06d4f5827f8d798df90
Judith <jk.otten@outlook.com>
2020-04-06 15:44:53 +0200

    Database connectie zit erbij

--- dal.py
+++ dal.py
@@ -1,2 +1,30 @@
+import mysql.connector
+from mysql.connector import Error
+
 class Dal:
     pass
+
+
+##testje voor database connectie
+try:
+    connection = mysql.connector.connect(
+      host="localhost",
+      database='casus_voorraden',
+      user="root",
+      passwd="root", auth_plugin='mysql_native_password'
+    )
+
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("You're connected to database: ", record)
+
+except Error as e:
+    print("Error while connecting to MySQL", e)
+finally:
+    if connection.is_connected():
+        connection.close()
+        print("Database is closed")

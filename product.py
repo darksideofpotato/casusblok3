6eba1fe7ea5ca988ff213775f1684ab882033c81
Judith <33284318+darksideofpotato@users.noreply.github.com>
2020-04-14 14:57:26 +0200

    product class

---
+++ product.py
@@ -0,0 +1,9 @@
+class Product:
+    def __init__(self, productID, leverancierID, productnaam, inkoopprijs,  voorraad, min,max):
+        self.productID = productID
+        self.leverancierID = leverancierID
+        self.productnaam = productnaam
+        self.inkoopprijs = inkoopprijs
+        self.voorraad = voorraad
+        self.min = min
+        self.max = max

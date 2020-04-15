411ffc39c753f410c71fbe5e046a1d00167bc140
Judith <33284318+darksideofpotato@users.noreply.github.com>
2020-04-15 16:01:26 +0200

    max te orderen producten bij handmatig bestellen ingebouwd

--- product_handler.py
+++ product_handler.py
@@ -18,7 +18,7 @@ class Producthandler:
             for x in y:
                 x = '{:15}'.format(str(x))
                 print(x, end=" ")
-        print("\n")
+            print("\n")
         for product in self.result:
             counter = counter + 1
             print(str(counter) + ". " + str(product))
@@ -31,11 +31,18 @@ class Producthandler:
 
                 if chosen_product != "":
                     chosen_product = int(chosen_product) - 1
+                    current_quantity = self.result[chosen_product][4]
+                    max_quantity = self.result[chosen_product][6]
+                    allowed_to_order = max_quantity - current_quantity
+                    hoeveelheid = input("Hoeveel stuks wil je bestellen? (max " + str(allowed_to_order) + ".)")
 
-                    hoeveelheid = input("Hoeveel stuks wil je bestellen?")
-                    list_of_items[(self.result[chosen_product][0])] = hoeveelheid
+                    if int(hoeveelheid) <= int(allowed_to_order):
 
-                    print("item " + self.result[chosen_product][1] + " toegevoegd aan order!")
+                        list_of_items[(self.result[chosen_product][0])] = hoeveelheid
+                        print("item " + self.result[chosen_product][1] + " toegevoegd aan order!")
+                    else:
+                        print("Je probeert meer te bestellen dan de maximum voorraad toelaat. Probeer minder"
+                              "te bestellen.")
                 else:
                     flag = False
                     return list_of_items
